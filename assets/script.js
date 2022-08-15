94ae36286eefb6db410073e0c92e1c8c3e410237
isayani <isayani95@gmail.com>
2022-08-14 20:13:39 -0400

    iterated 5 day fixed uv

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,11 @@
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
+var cityArr = []
+if (localStorage.getItem("Cities")) {
+    cityArr = JSON.parse(localStorage.getItem("Cities"))
+}
+
 var weather = {
     "apiKey": "f06df96322709f9b0254307f0735bb9c",
     fetchWeather: function (city) {
@@ -25,43 +30,68 @@ var weather = {
         .then((data) => this.displayWeather(data));
     },
 
-    fetchUVI: function (lat, lon) {
-        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.749&lon=-84.388&units=imperial&appid=" 
+    fetchUVI: function (data) {
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat="
+        + data.coord.lat
+        + "&lon="
+        + data.coord.lon
+        + "&units=imperial&appid=" 
         + this.apiKey
         )
         .then((response) => response.json())
-        .then((data) => this.displayUVI(data));
+        .then((data) => {
+            this.fiveDay(data)
+            this.displayUVI(data)});
     },
 
     displayWeather: function (data) {
         var { name } = data;
+        if (cityArr.includes(name) === false) {
+            cityArr.push(name)
+            localStorage.setItem("Cities", JSON.stringify(cityArr))
+        }
+        
         var { icon, description } = data.weather[0];
         var { temp, humidity } = data.main;
         var { speed } = data.wind;
-        // var { uvi } = data.current.uvi;
         // console.log(name, dt, icon, description, temp, humidity, speed)
         document.getElementById("city").innerText = name;
         document.getElementById("temp").innerText = temp + "°F";
         document.getElementById("descr").innerText = description;
         document.getElementById("icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.getElementById("humid").innerText = "Humidity: " + humidity + "%";
-        document.getElementById("speed").innerText = "Wind Speed: " + speed + "mph"; 
+        document.getElementById("speed").innerText = "Wind Speed: " + speed + "mph";
+        this.fetchUVI(data)
     },
     search: function () {
-        this.fetchWeather(document.querySelector(".searchBar").value);
+        this.fetchWeather(document.querySelector(".searchBar").value); 
     },
 
     displayUVI: function (data) {
         var { uvi } = data.daily[0];
-        // console.log(uvi) - works for ATL
         document.getElementById("uvi").innerText = "UV Index: " + uvi;
     },
-    search: function () {
-        this.fetchUVI(document.querySelector(".searchBar").value);
-    }
 
-    
+    fiveDay: function (data) {
+        var day = document.querySelector(".fiveDay");
+        day.innerHTML = "";
+        for (var i = 1; i < 6; i++) {
+            day.innerHTML += `<div class="day1 w-1/4  m-2 card">
+            <h2 id="temp">${data.daily[i].temp.day}°</h2>
+            <div class="flex">
+                <p id="descr">${data.daily[i].weather[0].description}</p>
+                <img id="icon" src="https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}.png" alt="">
+            </div>
+            <p id="humid">Humidity: ${data.daily[i].humidity}%</p>
+            <p id="speed">Wind Speed: ${data.daily[i].wind_speed} mph</p>
+        </div>`
+        }
+
+    }
 };
+
+
+
 
 document.querySelector(".btn").addEventListener("click", function () {
     weather.search();

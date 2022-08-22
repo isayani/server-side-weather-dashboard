3c8b03c3b3c14bd963de1dd803d8323d131ef395
isayani <isayani95@gmail.com>
2022-08-21 20:26:44 -0400

    fixed uvi color and styling, 5-day date, icon, made search buttons

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 // current date
 var dateEl = document.getElementById("date");
-var moment = moment();
-dateEl.innerText = moment.format("MMMM Do YYYY");
+dateEl.innerText = moment().format("MMMM Do YYYY");
 
 
 // create an array in local storage from Cities
@@ -14,7 +13,7 @@ if (localStorage.getItem("Cities")) {
 function generateListItems (cityArr) {
     var cities = "";
     for (let i = 0; i < cityArr.length; i++) {
-        cities += `<li>${cityArr[i]}</li>`;
+        cities += `<button class="prevCities">${cityArr[i]}</button>`;
     }
     return cities;
 }
@@ -78,12 +77,13 @@ var weather = {
 
     displayUVI: function (data) {
         var { uvi } = data.daily[0];
-        document.getElementById("uvi").innerText = "UV Index: " + uvi;
+        document.getElementById("uvi").innerText = uvi;
+
 
         // color code based on uv index
-        if (0 < uvi <= 5) {
+        if ( uvi > 0 && uvi < 5) {
             document.getElementById("uvi").setAttribute('style', "background-color: rgba(0, 128, 0, 0.419);");
-        } else if (5 < uvi <= 7) {
+        } else if (uvi > 5 && uvi < 7) {
             document.getElementById("uvi").setAttribute('style', "background-color: rgba(113, 128, 0, 0.419);")
         } else {
             document.getElementById("uvi").setAttribute('style', "background-color: rgba(128, 23, 0, 0.419);")
@@ -94,14 +94,16 @@ var weather = {
     fiveDay: function (data) {
         console.log("this is the data",data); 
         var day = document.querySelector(".fiveDay");
+        
         day.innerHTML = "";
         for (var i = 1; i < 6; i++) {
+            console.log(moment(data.daily[i].dt,"X").format("MMMM Do YYYY"));
             day.innerHTML += `<div class="day1 w-1/4  m-2 card">
-            <div id="fiveDate">${moment.add(24, "hours").format("MMMM Do YYYY")}</div>
+            <div id="fiveDate">${moment(data.daily[i].dt,"X").format("MMMM Do YYYY")}</div>
             <h2 id="temp">${data.daily[i].temp.day}°</h2>
             <div class="flex">
-                <p id="descr">${data.daily[i].weather[0].description}</p>
-                <img id="icon" src="https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}.png" alt="">
+                <p class="iconDescr" id="descr">${data.daily[i].weather[0].description}
+                <img class="iconDescr" id="icon" src="https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}.png" alt=""></p>
             </div>
             <p id="humid">Humidity: ${data.daily[i].humidity}%</p>
             <p id="speed">Wind Speed: ${data.daily[i].wind_speed} mph</p>
@@ -111,6 +113,12 @@ var weather = {
     }
 };
 
+// add Event Listener to search hist
+var searchCities = $(".prevCities")
+searchCities.on("click", function() {
+    var cityName = $(this).text()
+    weather.fetchWeather(cityName)
+})
 
 // key and click event listeners
 document.querySelector(".btn").addEventListener("click", function () {
